8ae1aa46784c98c4cf475db866ffd70387c1a8c6
Essan'Utep Blue <personalvestige@gmail.com>
2021-02-25 15:41:40 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Setup canvas
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// Function to generate random number
+
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
